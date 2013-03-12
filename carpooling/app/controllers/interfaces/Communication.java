7a500d4db20413f4300b1eeb7d2bbe77822df41d
Denis Michiels <dmichiels@gmail.com>
2013-03-12 13:11:42 +0100

    modification de l'interface Communication (TrajectFound ne prend plus de trajet en argument)

--- carpooling/app/controllers/interfaces/Communication.java
+++ carpooling/app/controllers/interfaces/Communication.java
@@ -28,7 +28,7 @@ abstract class Communication{
 	 * est finalement trouve plus tard, alors l'utilisateur est 
 	 * prevenu.
 	 */
-	public static void trajectFound(User user, Traject t){
+	public static void trajectFound(User user){
 		
 	}
 	
