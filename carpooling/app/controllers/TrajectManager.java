4115055cda5d7123e9c2acdc9b47b870e52eb7ce
Denis Michiels <dmichiels@gmail.com>
2013-03-13 09:30:59 +0100

    add TrajectManager

---
+++ carpooling/app/controllers/TrajectManager.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import models.objects.Traject;
+import models.objects.User;
+import controllers.interfaces.ITrajectManager;
+
+public class TrajectManager implements ITrajectManager {
+
+	@Override
+	public void recordTraject(Traject traj, User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cancelTraject(Traject traj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void arrivalNotification(Traject traj, short rating) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
