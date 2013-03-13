ac4059ff02b829133e7a1e8079231273a55eed63
Sébastien De Fauw <sebastien.defauw@student.uclouvain.be>
2013-03-13 22:56:14 +0100

    Add constructors and toString

--- carpooling/app/models/objects/Coordinate.java
+++ carpooling/app/models/objects/Coordinate.java
@@ -1,23 +1,45 @@
 package models.objects;
 
 public class Coordinate {
-	private float x, y;
+    private float x, y;
 
-	public float getX() {
-		return x;
-	}
+    public Coordinate() {
+    }
 
-	public void setX(float x) {
-		this.x = x;
-	}
+    public Coordinate(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public float getY() {
-		return y;
-	}
+    /**
+     * Transform a string to coordinate
+     *
+     * @param coord String format: FLOAT,FLOAT
+     */
+    public Coordinate(String coord) {
+        String[] c = coord.split(",");
+        this.x = Float.parseFloat(c[0]);
+        this.y = Float.parseFloat(c[1]);
+    }
 
-	public void setY(float y) {
-		this.y = y;
-	}
-	
-	
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public String toString() {
+        return this.x + "," + this.y;
+    }
+
 }
