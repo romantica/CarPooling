9f5cf1a7a61842b346979e90c5c2e73e77b14b63
Olivier de Harenne <olivierdeh@hotmail.com>
2013-03-14 10:37:13 +0100

    Small update to request form

--- carpooling/app/controllers/RequestUI.java
+++ carpooling/app/controllers/RequestUI.java
@@ -12,10 +12,22 @@ import views.html.*;
  * Date: 13/03/13
  */
 public class RequestUI extends Controller {
+
 	public static Result create() {
 		Login sess = new Login();
 		// if (!session.isLogged())
            // return redirect("/");
 		return ok(requestcreate.render(sess.getUsername(), null));
+	}
+	
+	public static Result selectTraject() {
+		Login session = new Login();
+        // if (!session.isLogged())
+            // return redirect("/");
+		
+		DynamicForm data = Form.form().bindFromRequest();
+		
+		
+		return ok("Data received: time:"+data.get("arrivalTime"));
 	}
 }
