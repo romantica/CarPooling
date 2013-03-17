ac524df870be6c81ba8b57e6f4d90935b97fc528
Sébastien De Fauw <sebastien.defauw@student.uclouvain.be>
2013-03-17 10:22:11 +0100

    DB OK: /!\ IL FAUT RECREER TT LA DB

--- DB/build.sql
+++ DB/build.sql
@@ -2,246 +2,234 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-DROP SCHEMA IF EXISTS `CarPooling` ;
-CREATE SCHEMA IF NOT EXISTS `CarPooling` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-DROP SCHEMA IF EXISTS `carpooling` ;
 CREATE SCHEMA IF NOT EXISTS `carpooling` DEFAULT CHARACTER SET utf8 ;
-USE `CarPooling` ;
+USE `carpooling` ;
 
 -- -----------------------------------------------------
--- Table `CarPooling`.`User`
+-- Table `carpooling`.`User`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`User` ;
-
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`User` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
-  `Login` VARCHAR(45) ,
-  `Password` VARCHAR(45) ,
-  `LastName` VARCHAR(45) COMMENT '	' ,
-  `FirstName` VARCHAR(45) ,
-  `AccountNumber` INT NULL ,
-  `PhoneNumber` VARCHAR(45) NULL ,
-  `Email` VARCHAR(45) ,
+CREATE  TABLE IF NOT EXISTS `carpooling`.`User` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT ,
+  `Login` VARCHAR(45) NOT NULL ,
+  `Password` VARCHAR(45) NOT NULL ,
+  `LastName` VARCHAR(45) NOT NULL COMMENT '	' ,
+  `FirstName` VARCHAR(45) NOT NULL ,
+  `AccountNumber` INT(11) NULL DEFAULT NULL ,
+  `PhoneNumber` VARCHAR(45) NULL DEFAULT NULL ,
+  `Email` VARCHAR(45) NULL DEFAULT NULL ,
   PRIMARY KEY (`ID`) ,
   UNIQUE INDEX `Login_UNIQUE` (`Login` ASC) )
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `CarPooling`.`Car`
+-- Table `carpooling`.`Account`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`Car` ;
+CREATE  TABLE IF NOT EXISTS `carpooling`.`Account` (
+  `UserID` INT(11) NOT NULL ,
+  `Balance` INT(11) NOT NULL DEFAULT 0 ,
+  PRIMARY KEY (`UserID`) ,
+  UNIQUE INDEX `UserID_UNIQUE` (`UserID` ASC) ,
+  CONSTRAINT `Prossessing`
+    FOREIGN KEY (`UserID` )
+    REFERENCES `carpooling`.`User` (`ID` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`Car` (
+
+-- -----------------------------------------------------
+-- Table `carpooling`.`Assessment`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `carpooling`.`Assessment` (
+  `UserID` INT(11) NOT NULL ,
+  `Rating` SMALLINT(6) NOT NULL ,
+  `Comment` LONGTEXT NULL DEFAULT NULL ,
+  `Type` TINYINT(1) NULL DEFAULT NULL ,
+  PRIMARY KEY (`UserID`) ,
+  INDEX `Grading_idx` (`UserID` ASC) ,
+  CONSTRAINT `Grading`
+    FOREIGN KEY (`UserID` )
+    REFERENCES `carpooling`.`User` (`ID` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `carpooling`.`Car`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `carpooling`.`Car` (
   `PlateNumber` VARCHAR(7) NOT NULL ,
-  `UserID` INT ,
-  `Model` VARCHAR(45) NULL ,
-  `Color` VARCHAR(45) NULL ,
+  `UserID` INT(11) NOT NULL ,
+  `Model` VARCHAR(45) NULL DEFAULT NULL ,
+  `Color` VARCHAR(45) NULL DEFAULT NULL ,
   PRIMARY KEY (`PlateNumber`) ,
   INDEX `Owning_idx` (`UserID` ASC) ,
   CONSTRAINT `Owning`
     FOREIGN KEY (`UserID` )
-    REFERENCES `CarPooling`.`User` (`ID` )
+    REFERENCES `carpooling`.`User` (`ID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `CarPooling`.`Proposal`
+-- Table `carpooling`.`PickupPoint`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`Proposal` ;
+CREATE  TABLE IF NOT EXISTS `carpooling`.`PickupPoint` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT ,
+  `Name` VARCHAR(45) NULL DEFAULT NULL ,
+  `CoordinateX` DOUBLE NOT NULL ,
+  `CoordinateY` DOUBLE NOT NULL ,
+  `Address` VARCHAR(45) NULL DEFAULT NULL ,
+  `Description` LONGTEXT NULL DEFAULT NULL ,
+  PRIMARY KEY (`ID`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`Proposal` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
-  `UserID` INT ,
-  `CarID` VARCHAR(7) ,
-  `KmCost` FLOAT ,
-  `AvailableSeats` TINYINT ,
+
+-- -----------------------------------------------------
+-- Table `carpooling`.`Request`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `carpooling`.`Request` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT ,
+  `UserID` INT(11) NOT NULL ,
+  `DepartureCoordinateX` DOUBLE NOT NULL ,
+  `DepartureCoordinateY` DOUBLE NOT NULL ,
+  `DepartureAddress` VARCHAR(45) NOT NULL ,
+  `ArrivalCoordinateX` DOUBLE NOT NULL ,
+  `ArrivalCoordinateY` DOUBLE NOT NULL ,
+  `ArrivalAddress` VARCHAR(45) NOT NULL ,
+  `NecessarySeats` TINYINT(4) NOT NULL ,
+  `ToleranceTime` INT(11) NOT NULL ,
+  `ToleranceWalkDistance` INT(11) NOT NULL ,
+  `TolerancePrice` INT(11) NOT NULL ,
+  PRIMARY KEY (`ID`) ,
+  INDEX `Submitting_idx` (`UserID` ASC) ,
+  CONSTRAINT `Submitting`
+    FOREIGN KEY (`UserID` )
+    REFERENCES `carpooling`.`User` (`ID` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `carpooling`.`Proposal`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `carpooling`.`Proposal` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT ,
+  `UserID` INT(11) NOT NULL ,
+  `CarID` VARCHAR(7) NOT NULL ,
+  `KmCost` FLOAT NOT NULL ,
+  `AvailableSeats` TINYINT(4) NOT NULL ,
   PRIMARY KEY (`ID`) ,
   INDEX `Proposing_idx` (`UserID` ASC) ,
   INDEX `Using_idx` (`CarID` ASC) ,
   CONSTRAINT `Proposing`
     FOREIGN KEY (`UserID` )
-    REFERENCES `CarPooling`.`User` (`ID` )
+    REFERENCES `carpooling`.`User` (`ID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `Using`
     FOREIGN KEY (`CarID` )
-    REFERENCES `CarPooling`.`Car` (`PlateNumber` )
+    REFERENCES `carpooling`.`Car` (`PlateNumber` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `CarPooling`.`Account`
+-- Table `carpooling`.`Traject`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`Account` ;
-
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`Account` (
-  `UserID` INT NOT NULL ,
-  `Balance` INT ,
-  UNIQUE INDEX `UserID_UNIQUE` (`UserID` ASC) ,
-  PRIMARY KEY (`UserID`) ,
-  CONSTRAINT `Prossessing`
-    FOREIGN KEY (`UserID` )
-    REFERENCES `CarPooling`.`User` (`ID` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `CarPooling`.`Assessment`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`Assessment` ;
-
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`Assessment` (
-  `UserID` INT NOT NULL ,
-  `Rating` SMALLINT ,
-  `Comment` LONGTEXT NULL ,
-  `Type` TINYINT(1) ,
-  INDEX `Grading_idx` (`UserID` ASC) ,
-  PRIMARY KEY (`UserID`) ,
-  CONSTRAINT `Grading`
-    FOREIGN KEY (`UserID` )
-    REFERENCES `CarPooling`.`User` (`ID` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `CarPooling`.`Request`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`Request` ;
-
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`Request` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
-  `UserID` INT ,
-  `DepartureCoordinateX` DOUBLE ,
-  `DepartureCoordinateY` DOUBLE ,
-  `DepartureAddress` VARCHAR(45) ,
-  `ArrivalCoordinateX` DOUBLE ,
-  `ArrivalCoordinateY` DOUBLE ,
-  `ArrivalAddress` VARCHAR(45) ,
-  `NecessarySeats` TINYINT ,
-  `ToleranceTime` INT NULL ,
-  `ToleranceWalkDistance` INT NULL ,
-  `TolerancePrice` INT NULL ,
-  PRIMARY KEY (`ID`) ,
-  INDEX `Submitting_idx` (`UserID` ASC) ,
-  CONSTRAINT `Submitting`
-    FOREIGN KEY (`UserID` )
-    REFERENCES `CarPooling`.`User` (`ID` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `CarPooling`.`Traject`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`Traject` ;
-
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`Traject` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
-  `UserID` INT NULL ,
-  `RequestID` INT ,
-  `ProposalID` INT ,
-  `RerservedSeats` TINYINT ,
-  `TotalCost` FLOAT ,
+CREATE  TABLE IF NOT EXISTS `carpooling`.`Traject` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT ,
+  `UserID` INT(11) NOT NULL ,
+  `RequestID` INT(11) NOT NULL ,
+  `ProposalID` INT(11) NOT NULL ,
+  `RerservedSeats` TINYINT(4) NOT NULL ,
+  `TotalCost` FLOAT NOT NULL ,
   PRIMARY KEY (`ID`) ,
   INDEX `Chossing_idx` (`UserID` ASC) ,
   INDEX `Corresponding_idx` (`RequestID` ASC) ,
   INDEX `Including_idx` (`ProposalID` ASC) ,
   CONSTRAINT `Chossing`
     FOREIGN KEY (`UserID` )
-    REFERENCES `CarPooling`.`User` (`ID` )
+    REFERENCES `carpooling`.`User` (`ID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `Corresponding`
     FOREIGN KEY (`RequestID` )
-    REFERENCES `CarPooling`.`Request` (`ID` )
+    REFERENCES `carpooling`.`Request` (`ID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `Including`
     FOREIGN KEY (`ProposalID` )
-    REFERENCES `CarPooling`.`Proposal` (`ID` )
+    REFERENCES `carpooling`.`Proposal` (`ID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `CarPooling`.`PickupPoint`
+-- Table `carpooling`.`Composition`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`PickupPoint` ;
-
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`PickupPoint` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
-  `Description` VARCHAR(45) NULL ,
-  `Name` VARCHAR(45) NULL ,
-  `CoordinateX` DOUBLE ,
-  `CoordinateY` DOUBLE ,
-  `Address` VARCHAR(45) ,
-  PRIMARY KEY (`ID`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `CarPooling`.`Itineray`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`Itinerary` ;
-
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`Itinerary` (
-  `PickupPointID` INT NOT NULL ,
-  `ProposalID` INT NOT NULL ,
-  `DepartureTime` DATETIME NULL ,
-  `ArrivalTime` DATETIME NULL ,
-  INDEX `pp_idx` (`PickupPointID` ASC) ,
-  INDEX `prop_idx` (`ProposalID` ASC) ,
-  PRIMARY KEY (`PickupPointID`, `ProposalID`) ,
-  CONSTRAINT `pp`
-    FOREIGN KEY (`PickupPointID` )
-    REFERENCES `CarPooling`.`PickupPoint` (`ID` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `prop`
-    FOREIGN KEY (`ProposalID` )
-    REFERENCES `CarPooling`.`Proposal` (`ID` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `CarPooling`.`Composition`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `CarPooling`.`Composition` ;
-
-CREATE  TABLE IF NOT EXISTS `CarPooling`.`Composition` (
-  `PickupPointID` INT NOT NULL ,
-  `TrajectID` INT NOT NULL ,
-  `Type` TINYINT(1) ,
-  `Time` DATETIME NULL ,
+CREATE  TABLE IF NOT EXISTS `carpooling`.`Composition` (
+  `PickupPointID` INT(11) NOT NULL ,
+  `TrajectID` INT(11) NOT NULL ,
+  `Type` TINYINT(1) NOT NULL ,
+  `Time` DATETIME NOT NULL ,
+  PRIMARY KEY (`PickupPointID`, `TrajectID`) ,
   INDEX `traject_idx` (`TrajectID` ASC) ,
   INDEX `pp_idx` (`PickupPointID` ASC) ,
-  PRIMARY KEY (`PickupPointID`, `TrajectID`) ,
   CONSTRAINT `pp2`
     FOREIGN KEY (`PickupPointID` )
-    REFERENCES `CarPooling`.`PickupPoint` (`ID` )
+    REFERENCES `carpooling`.`PickupPoint` (`ID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `traject`
     FOREIGN KEY (`TrajectID` )
-    REFERENCES `CarPooling`.`Traject` (`ID` )
+    REFERENCES `carpooling`.`Traject` (`ID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `carpooling`.`Itinerary`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `carpooling`.`Itinerary` (
+  `PickupPointID` INT(11) NOT NULL ,
+  `ProposalID` INT(11) NOT NULL ,
+  `DepartureTime` DATETIME NOT NULL ,
+  `ArrivalTime` DATETIME NOT NULL ,
+  PRIMARY KEY (`PickupPointID`, `ProposalID`) ,
+  INDEX `pp_idx` (`PickupPointID` ASC) ,
+  INDEX `prop_idx` (`ProposalID` ASC) ,
+  CONSTRAINT `pp`
+    FOREIGN KEY (`PickupPointID` )
+    REFERENCES `carpooling`.`PickupPoint` (`ID` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `prop`
+    FOREIGN KEY (`ProposalID` )
+    REFERENCES `carpooling`.`Proposal` (`ID` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 USE `carpooling` ;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- carpooling/conf/evolutions/default/1.sql
+++ carpooling/conf/evolutions/default/1.sql
@@ -40,18 +40,19 @@ create table itinerary (
   constraint pk_itinerary primary key (id))
 ;
 
-create table pickup_point (
+create table PickupPoint (
   id                        integer auto_increment not null,
   name                      varchar(255),
   description               varchar(255),
   address                   varchar(255),
-  constraint pk_pickup_point primary key (id))
+  constraint pk_PickupPoint primary key (id))
 ;
 
 create table proposal (
   id                        integer auto_increment not null,
   km_cost                   float,
   available_seats           integer,
+  user_id                   integer,
   constraint pk_proposal primary key (id))
 ;
 
@@ -86,7 +87,59 @@ create table user (
 ;
 
 
+create table proposal_traject (
+  proposal_id                    integer not null,
+  traject_id                     integer not null,
+  constraint pk_proposal_traject primary key (proposal_id, traject_id))
+;
 
+create table user_car (
+  user_id                        integer not null,
+  car_id                         integer not null,
+  constraint pk_user_car primary key (user_id, car_id))
+;
+
+create table user_proposal (
+  user_id                        integer not null,
+  proposal_id                    integer not null,
+  constraint pk_user_proposal primary key (user_id, proposal_id))
+;
+
+create table user_traject (
+  user_id                        integer not null,
+  traject_id                     integer not null,
+  constraint pk_user_traject primary key (user_id, traject_id))
+;
+
+create table user_request (
+  user_id                        integer not null,
+  request_id                     integer not null,
+  constraint pk_user_request primary key (user_id, request_id))
+;
+alter table proposal add constraint fk_proposal_user_1 foreign key (user_id) references user (id) on delete restrict on update restrict;
+create index ix_proposal_user_1 on proposal (user_id);
+
+
+
+alter table proposal_traject add constraint fk_proposal_traject_proposal_01 foreign key (proposal_id) references proposal (id) on delete restrict on update restrict;
+
+alter table proposal_traject add constraint fk_proposal_traject_traject_02 foreign key (traject_id) references traject (id) on delete restrict on update restrict;
+
+alter table user_car add constraint fk_user_car_user_01 foreign key (user_id) references user (id) on delete restrict on update restrict;
+
+alter table user_car add constraint fk_user_car_car_02 foreign key (car_id) references car (id) on delete restrict on update restrict;
+
+alter table user_proposal add constraint fk_user_proposal_user_01 foreign key (user_id) references user (id) on delete restrict on update restrict;
+
+alter table user_proposal add constraint fk_user_proposal_proposal_02 foreign key (proposal_id) references proposal (id) on delete restrict on update restrict;
+
+alter table user_traject add constraint fk_user_traject_user_01 foreign key (user_id) references user (id) on delete restrict on update restrict;
+
+alter table user_traject add constraint fk_user_traject_traject_02 foreign key (traject_id) references traject (id) on delete restrict on update restrict;
+
+alter table user_request add constraint fk_user_request_user_01 foreign key (user_id) references user (id) on delete restrict on update restrict;
+
+alter table user_request add constraint fk_user_request_request_02 foreign key (request_id) references request (id) on delete restrict on update restrict;
 
 # --- !Downs
 
@@ -102,15 +155,25 @@ drop table coordinate;
 
 drop table itinerary;
 
-drop table pickup_point;
+drop table PickupPoint;
 
 drop table proposal;
+
+drop table proposal_traject;
 
 drop table request;
 
 drop table traject;
 
 drop table user;
+
+drop table user_car;
+
+drop table user_proposal;
+
+drop table user_traject;
+
+drop table user_request;
 
 SET FOREIGN_KEY_CHECKS=1;
 
