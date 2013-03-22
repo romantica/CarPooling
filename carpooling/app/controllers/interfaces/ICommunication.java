5706d15ceddeaf2b07a3ba1ec1c48c6d86f4505a
aroyers <amaury.royers@gmail.com>
2013-03-22 09:45:42 +0100

    Messages des emails

--- carpooling/app/controllers/interfaces/ICommunication.java
+++ carpooling/app/controllers/interfaces/ICommunication.java
@@ -15,8 +15,15 @@ public abstract class ICommunication{
 	 */
 	public static void RequestCancelled(User driver, Traject t){
 		String dest = driver.getEmail();
+		int remainingSeats = t.getReservedSeats()-1;
 		String subject = "[UCL Carpooling] Request cancelled";
-		String message = "";
+		String message = "Hello,\n\nThe user "+t.getRequest().getUser().getLogin()+"cancelled its request for the following proposal :\n\nDeparture address : "
+							+t.getDeparturePP().getPickupPoint().getAddress()+"\nArrival address : "+t.getArrivalPP().getPickupPoint().getAddress()
+							+"\nDeparture time : "+t.getProposal().getItinerary(t.getDeparturePP().getPickupPoint()).getDepartureTime().toString()
+							+"\nRemaining reserved seats : "+remainingSeats;
+	    if(remainingSeats <= 0)
+	    	message = message+"\n\nThe corresponding traject has been cancelled.";
+		message = message+"\n\nThe UCL Carpooling team";
 		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
 		mail.setSubject(subject);
 		mail.addRecipient(dest);
@@ -27,7 +34,10 @@ public abstract class ICommunication{
 	public static void ProposalCancelled(User passenger, Traject t){
 		String dest = passenger.getEmail();
 		String subject = "[UCL Carpooling] Proposal cancelled";
-		String message = "";
+		String message = "Hello,\n\nThe user "+t.getRequest().getUser().getLogin()+"cancelled the following proposal :\n\nDeparture address : "
+							+t.getDeparturePP().getPickupPoint().getAddress()+"\nArrival address : "+t.getArrivalPP().getPickupPoint().getAddress()
+							+"\nDeparture time : "+t.getProposal().getItinerary(t.getDeparturePP().getPickupPoint()).getDepartureTime().toString()
+							+"\n\nThe corresponding traject is cancelled.\n\nThe UCL Carpooling team";
 		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
 		mail.setSubject(subject);
 		mail.addRecipient(dest);
@@ -42,7 +52,12 @@ public abstract class ICommunication{
 	 */
 	public static void trajectReminder(User[] passenger, User driver, Traject t){
 		String subject = "[UCL Carpooling] Traject Reminder";
-		String message = "";
+		String message = "Hello,\n\nThe following traject is about to start :\n\nDeparture address : "
+							+t.getDeparturePP().getPickupPoint().getAddress()+"\nArrival address : "+t.getArrivalPP().getPickupPoint().getAddress()
+							+"\nReserved seats : "+t.getReservedSeats()
+							+"\nDeparture time : "+t.getProposal().getItinerary(t.getDeparturePP().getPickupPoint()).getDepartureTime().toString()
+							+"\nArrival time : "+t.getProposal().getItinerary(t.getArrivalPP().getPickupPoint()).getArrivalTime().toString()
+							+"\n\nThe UCL Carpooling team";
 		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
 		for(int i = 0; i < passenger.length; i++){
 			mail.addRecipient(passenger[i].getEmail());
@@ -50,7 +65,7 @@ public abstract class ICommunication{
 		mail.addRecipient(driver.getEmail());
 		mail.setSubject(subject);
 		mail.addFrom(sender);
-		mail.send("");
+		mail.send(message);
 	}
 	
 	/**
@@ -61,7 +76,7 @@ public abstract class ICommunication{
 	public static void trajectFound(User user){
 		String dest = user.getEmail();
 		String subject = "[UCL Carpooling] Traject found";
-		String message = "";
+		String message = "Hello,\n\nA new traject corresponding to your request has been created. Please go to www.uclcarpooling.be to get all details.\n\nThe UCL Carpooling team";
 		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
 		mail.setSubject(subject);
 		mail.addRecipient(dest);
@@ -74,9 +89,14 @@ public abstract class ICommunication{
 	 * recoit les informations qui permettront le ramassage.
 	 */
 	public static void newPassengerNotification(Traject t){
-		String dest = t.getProposal().getUser().getEmail();
+		String dest = t.getUser().getEmail();
 		String subject = "[UCL Carpooling] New passenger registered";
-		String message = "";
+		String message = "Hello,\n\nA new passenger has been added to the following traject :\n\nDeparture address : "
+							+t.getDeparturePP().getPickupPoint().getAddress()+"\nArrival address : "+t.getArrivalPP().getPickupPoint().getAddress()
+							+"\nReserved seats : "+t.getReservedSeats()
+							+"\nDeparture time : "+t.getProposal().getItinerary(t.getDeparturePP().getPickupPoint()).getDepartureTime().toString()
+							+"\nArrival time : "+t.getProposal().getItinerary(t.getArrivalPP().getPickupPoint()).getArrivalTime().toString()
+							+"\n\nThe UCL Carpooling team";
 		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
 		mail.setSubject(subject);
 		mail.addRecipient(dest);
@@ -90,7 +110,8 @@ public abstract class ICommunication{
 	public static void newUser(User u){
 		String dest = u.getEmail();
 		String subject = "[UCL Carpooling] Account creation";
-		String message = "";
+		String message = "Hello, \n\nA new account has been created on www.uclcarpooling.be with the following information :\n\nLogin : "
+							+u.getLogin()+"\nEmail address : "+u.getEmail()+"\n\nThe UCL Carpooling team";
 		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
 		mail.setSubject(subject);
 		mail.addRecipient(dest);
@@ -104,7 +125,12 @@ public abstract class ICommunication{
 	public static void helpMeStaff(Traject t){
 		String dest = "staff@carpooling.uclouvain.be";
 		String subject = "[UCL Carpooling] Traject conflict";
-		String message = "";
+		String message = "Hello,\n\nThere is a conflict for the following traject :\n\nDeparture address : "
+							+t.getDeparturePP().getPickupPoint().getAddress()+"\nArrival address : "+t.getArrivalPP().getPickupPoint().getAddress()
+							+"\nReserved seats : "+t.getReservedSeats()
+							+"\nDeparture time : "+t.getProposal().getItinerary(t.getDeparturePP().getPickupPoint()).getDepartureTime().toString()
+							+"\nArrival time : "+t.getProposal().getItinerary(t.getArrivalPP().getPickupPoint()).getArrivalTime().toString()
+							+"\n\nPlease start investigating at soon as possible.\n\nThe UCL Carpooling team";
 		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
 		mail.setSubject(subject);
 		mail.addRecipient(dest);
