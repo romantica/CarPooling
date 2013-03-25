378916310ff75f8470f6c5528d6ceb430a8c85b0
Sébastien De Fauw <sebastien.defauw@student.uclouvain.be>
2013-03-25 11:52:41 +0100

    Traject (Passanger)

--- carpooling/app/controllers/TrajectUI.java
+++ carpooling/app/controllers/TrajectUI.java
@@ -1,8 +1,15 @@
 package controllers;
 
+import models.objects.Traject;
+import models.objects.User;
+import play.cache.Cache;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.traject.*;
+
+import java.util.List;
 
 public class TrajectUI  extends Controller {
 
@@ -17,6 +24,36 @@ public class TrajectUI  extends Controller {
         Login sess = new Login();
         if (!sess.isLogged())
             return redirect("/");
-        return ok(list.render(sess.getUsername(),null));
+        User user = UserManager.getUserLogged();
+        List<Traject> l = TrajectManager.getTrajects(user);
+        Cache.set("trjlist#" + user.getLogin(), l, 10 * 60);
+        return ok(list.render(sess.getUsername(),l));
+    }
+
+    public static Result remove(){
+        Login sess = new Login();
+        if (!sess.isLogged())
+            return redirect("/");
+        User user = UserManager.getUserLogged();
+        //Get id coming from get
+        DynamicForm form = Form.form().bindFromRequest();
+        int id = Integer.parseInt(form.get("id"));
+        //Get cache
+        List<Traject> trjList = (List<Traject>) Cache.get("trjlist#" + session("username"));
+        //Search traject to remove in cache with ID
+        Traject thistrj = null;
+        for(Traject trj: trjList){
+            if(trj.getId() == id){
+                thistrj = trj;
+            }
+        }
+        //If id is not in cache so it's a fake remove traject
+        if (thistrj == null)
+            return ok("FAKE");
+        //Remove traject
+        try{
+            TrajectManager.recordTraject(thistrj,user);
+        }catch (Exception e){}
+        return redirect("/traject/passanger");
     }
 }
